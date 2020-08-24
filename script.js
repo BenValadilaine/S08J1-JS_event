3a7f6de6342d18817acb4bc3384592264d7e084c
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-08-24 10:25:01 +0200

    exo1

---
+++ script.js
@@ -0,0 +1,13 @@
+//fonctionnalité 1
+
+let foot = document.querySelector("footer");
+let count = 0;
+console.log(foot);
+function footerClick() {
+  count ++;
+  console.log("clique numéro " + count)
+};
+foot.addEventListener("click", footerClick);
+
+
+//fonctionnalité 2
