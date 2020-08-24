483d0ee30d79e2c4c141da3cb5b40757032ab064
BenValadilaine <benjamin.valadilaine@gmail.com>
2020-08-24 16:55:13 +0200

    exo9

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ btnBackward.addEventListener("click", moveBackward);
 //fonctionnalité 9
 let btnLogo = document.querySelector(".navbar-brand");
 let container = document.querySelector(".album > .container");
+
 function moveCardsLeft(){
   let allCards = document.querySelectorAll(".col-md-4");
   console.log(allCards);
@@ -124,7 +125,47 @@ function moveCardsLeft(){
     container.classList.add("ml-0");
   });
 };
-// btnLogo.addEventListener("click ", moveCardsLeft);
+
+function moveCardsRight(){
+  let allCards = document.querySelectorAll(".col-md-4");
+  console.log(allCards);
+  allCards.forEach(card => {
+    card.classList.remove("col-md-4");
+    card.classList.add("col-md-3");
+    container.classList.add("mr-0");
+  });
+};
+
+function moveCardsCenter(){
+  let allCards = document.querySelectorAll(".col-md-4");
+  console.log(allCards);
+  allCards.forEach(card => {
+    card.classList.remove("col-md-4");
+    card.classList.add("col-md-3");
+    container.classList.add("mx-auto");
+  });
+};
+
+function resetCards(){
+  let allCards = document.querySelectorAll(".col-md-4");
+  console.log(allCards);
+  allCards.forEach(card => {
+    if (container.classList("mr-0")){
+      container.classList.remove("mr-0");
+      card.classList.remove("col-md-3");
+      card.classList.add("col-md-4");
+    } else if (container.classList("ml-0")){
+      container.classList.remove("ml-0")
+      card.classList.remove("col-md-3");
+      card.classList.add("col-md-4");
+    };
+  });
+};
+
+//Movign cards left
 ['click','keypress'].forEach( evt =>
-    btnLogo.addEventListener(evt, moveCardsLeft, false)
+  btnLogo.addEventListener(evt, moveCardsLeft, false)
 );
+
+//Moving cards right
+btnLogo.addEventListener("dblclick", moveCardsRight)
